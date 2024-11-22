7443318f3ac0a676a26e83a115cf7ed104306f54
Abdul Qadir <153851145+qadir2005@users.noreply.github.com>
2024-11-22 15:34:07 +0500

    changing

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,6 +7,7 @@ function Products() {
     const API_KEY = "https://dummyjson.com/products"
 
     const [loading,setLoading]=useState(true)
+    const [search, setSearch] = useState("")
 
     const [products,setProducts] = useState([])
     const getApiData = async ()=>{
@@ -18,7 +19,6 @@ function Products() {
         const Data = API_data?.data?.products
         
         setProducts(Data);
-        console.log(Data);
         
     setLoading(false)
           
@@ -32,14 +32,38 @@ function Products() {
       getApiData()  
 
     }, []) 
-      
 
+    const searchProducts = ()=>{
+      const result = products?.filter((item)=>{
+
+        return item.title.toLowerCase().includes(search)
+      }
+      )
+      return result || []
+    }
+    const priceLowToHigh = ()=>{
+      const ans =  products.sort((a,b)=>{
+        return a.price - b.price
+      })
+      return ans || []
+    }
      
   return (  
+    <div>
+      <div className='bg-slate-500 w-48' onClick={()=>priceLowToHigh
+      }>High to Low</div>
+      <div className='bg-slate-500 mt-4 w-48' onClick={()=>console.log("high")
+      }>Low to High</div>
+
+<input type="text" placeholder='search Products ' onChange={(e)=>{
+setSearch(e.target.value)
+
+} } className='bg-black h-10 w-[80%] my-auto pl-2  text-white' />
+
     <div className='flex flex-wrap  justify-center '>
       {loading?"loading":null}
       {
-        products.map((item)=>(
+        searchProducts().map((item)=>(
           <div key={item.id}>
             <img src={item.thumbnail} alt="" />
             <span style={item.availabilityStatus==="Low Stock" ? {color: "red" } :  {color:"blue"} }>
@@ -54,6 +78,7 @@ function Products() {
           </div>
         ))
     }
+    </div>
     </div>
   )
 }
